578e1360863336e16789b40d6ac2b5b2073217ad
Navneet Sharma <navneet123.sharma8@gmail.com>
2019-02-02 23:37:38 +0530

    added event listners

--- server/server.js
+++ server/server.js
@@ -1,12 +1,40 @@
 const path = require('path');
+const http = require('http');
 
 const express = require('express');
+var socketIO = require('socket.io');
 
 var publicPath = path.join(__dirname,'../public')
 var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
+var port = process.env.PORT || 3000;
 
 app.use(express.static(publicPath));
 
-app.listen(3000,() => {
-	console.log('Server is up and running on port 3000');
+io.on('connection', (socket) => {
+	console.log('new user connected');
+
+
+	socket.emit('newMessage', {
+		from: 'Server',
+		text: 'Hello!!! Client'
+	});
+
+
+	socket.on('createMessage', (message) => {
+		console.log(message);
+	});
+
+
+	socket.on('disconnect', () => {
+		console.log('user disconnected');
+	});
+});
+
+
+
+
+server.listen(3000,() => {
+	console.log(`Server is up and running on port ${port}`);
 });
